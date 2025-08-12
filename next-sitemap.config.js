2d53c0178a94f55a509f9921554aeb64a5e2f5dd
elsieok <157172705+elsieok@users.noreply.github.com>
2025-08-12 20:14:05 +0100

    sitemap stuff

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,10 @@
 export default {
   siteUrl: 'https://www.elsietheexplorer.com',
   generateRobotsTxt: true,
-  exclude:['/admin'],
+  exclude:['/admin', '/admin/*'],
   robotsTxtOptions: {
     policies: [
-      { userAgent: '*', disallow: ['/admin'] }, // block /admin from all bots
+      { userAgent: '*', disallow: ['/admin', '/admin/*'] }, // block /admin from all bots
     ],
   },
 };
