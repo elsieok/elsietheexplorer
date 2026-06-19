62fbc099a15dec84d3b21fe6106c8b18a4f26fc8
elsieok <157172705+elsieok@users.noreply.github.com>
2026-06-19 04:31:32 -0700

    chore: sitemap stuff

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -7,4 +7,13 @@ export default {
       { userAgent: '*', disallow: ['/admin', '/admin/*'] }, // block /admin from all bots
     ],
   },
+  additionalPaths: async (config) => {
+    return [
+      await config.transform(config, '/'),
+      await config.transform(config, '/about'),
+      await config.transform(config, '/blog'),
+      await config.transform(config, '/projects'),
+      await config.transform(config, '/photography'),
+    ];
+  },
 };
